d1d617ea536226594e7a7ab7ec4ba7e02c503fc6
Dave Bendit <david@ibendit.com>
2017-03-08 23:53:47 -0600

    Moving header to config, along with new footer

--- CalendarEventReceiver.py
+++ CalendarEventReceiver.py
@@ -33,7 +33,8 @@ def handler(event, context):
     credentials = Credentials.new_from_json(credentialJson)
     http_auth = credentials.authorize(httplib2.Http())
     calendar = build('calendar', 'v3', http=http_auth)
-    content = "The Maximum Overchill stream calendar has updated! Here are the streams for the next week:\n\n"
+    content = getConfigValue('header')
+    footer = getConfigValue('footer')
     for item in calendar.events().list(calendarId=calendarId, timeMin=now(), timeMax=week(), singleEvents=True, orderBy='startTime').execute()['items']:
         itemContent = ""
         itemContent = itemContent + 'Title:\t' + item['summary'] + '\n'
@@ -42,12 +43,12 @@ def handler(event, context):
         if ('description' in item):
             itemContent = itemContent + item['description'] + '\n'
         itemContent = itemContent + '\n'
-        if ((len(itemContent) + len(content)) >= 2000):
+        if ((len(itemContent) + len(content) + len(footer)) >= 2000):
             r = requests.post(discordWebhookUri, data={'content': content})
             content = itemContent
         else:
             content = content + itemContent
-    r = requests.post(discordWebhookUri, data={'content': content})
+    r = requests.post(discordWebhookUri, data={'content': content + footer})
 
 if __name__ == "__main__":
     handler(None, None)
